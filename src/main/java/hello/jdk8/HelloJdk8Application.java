aa82b52aecd98e0ba59c4b9ec8c806a8c551bc7c
DESKTOP-K61MA6V\85983 <xiezw@ruhrtec.cn>
2020-03-08 20:05:13 +0800

    hello jdk

---
+++ src/main/java/hello/jdk8/HelloJdk8Application.java
@@ -0,0 +1,13 @@
+package hello.jdk8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJdk8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloJdk8Application.class, args);
+    }
+
+}
