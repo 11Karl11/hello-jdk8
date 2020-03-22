daeba6fb6e4ed1b8abea0c02e7397b41d0493926
DESKTOP-K61MA6V\85983 <xiezw@ruhrtec.cn>
2020-03-22 15:41:14 +0800

    test stream

---
+++ src/main/java/stream/StreamTest11.java
@@ -0,0 +1,36 @@
+package stream;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * @Description TODO
+ * @Date 2020/3/22 11:49
+ * @Created karl xie
+ */
+public class StreamTest11 {
+    public static void main(String[] args) {
+        ArrayList<String> list = Lists.newArrayList("hello welcome", "world hello",
+                "hello world hello", "hello welcome");
+//        list.stream().map(item->item.split(" "))
+//                .distinct()
+//                .collect(Collectors.toList())
+//                .forEach(System.out::println);
+        list.stream().map(item->item.split(" "))
+                .flatMap(Arrays::stream)
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println("----------------");
+        list.stream().map(item->item.split(" "))
+                .flatMap(item->{
+                    ArrayList<String> list1 = Lists.newArrayList(item);
+                    return list1.stream();
+                })
+                .distinct()
+                .forEach(System.out::println);
+
+    }
+}
---
+++ src/main/java/stream/StreamTest8.java
@@ -0,0 +1,17 @@
+package stream;
+
+import java.util.stream.IntStream;
+
+/**
+ * @Description TODO
+ * @Date 2020/3/22 10:39
+ * @Created karl xie
+ */
+public class StreamTest8 {
+    public static void main(String[] args) {
+        IntStream.iterate(0,i->(i+1)%2)
+                .limit(6)
+                .distinct()
+                .forEach(System.out::println);
+    }
+}
