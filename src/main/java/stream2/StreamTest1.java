b1c4e2b88353a22d98b0da8a3324aae1b02e302e
DESKTOP-K61MA6V\85983 <xiezw@ruhrtec.cn>
2020-03-24 23:30:53 +0800

    test stream

--- src/main/java/stream2/StreamTest1.java
+++ src/main/java/stream2/StreamTest1.java
@@ -2,8 +2,7 @@ package stream2;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static java.util.stream.Collectors.*;
 
@@ -14,15 +13,68 @@ import static java.util.stream.Collectors.*;
  */
 public class StreamTest1 {
     public static void main(String[] args) {
-        Student zhangsan = new Student("zhangsan", 83);
+        Student zhangsan = new Student("zhangsan", 80);
         Student lisi = new Student("lisi", 90);
         Student wangwu = new Student("wangwu", 100);
         Student zhaoliu = new Student("zhaoliu", 90);
-        ArrayList<Student> students = Lists.newArrayList(zhangsan, lisi, wangwu, zhaoliu);
-        List<Student> students1 = students.stream().collect(toList());
-        students1.forEach(System.out::println);
-        System.out.println("--------------");
-        System.out.println(students.stream().collect(counting()));
-        System.out.println(students.stream().count());
+        Student zhaoliu1 = new Student("zhaoliu", 90);
+        ArrayList<Student> students = Lists.newArrayList(
+                zhangsan, lisi, wangwu, zhaoliu, zhaoliu1);
+//        List<Student> students1 = students.stream().collect(toList());
+//        students1.forEach(System.out::println);
+//        System.out.println("--------------");
+//        System.out.println(students.stream().collect(counting()));
+//        System.out.println(students.stream().count());
+//        students.stream().collect(
+//                minBy(Comparator.comparingInt(Student::getScore)))
+//                .ifPresent(System.out::println);
+//        students.stream().collect(
+//                maxBy(Comparator.comparingInt(Student::getScore))
+//        ).ifPresent(System.out::println);
+//        System.out.println(students.stream().collect(
+//                averagingInt(Student::getScore)
+//        ));
+//        System.out.println(students.stream().collect(
+//                summingInt(Student::getScore)
+//        ));
+//        System.out.println(students.stream().collect(
+//                summarizingInt(Student::getScore)
+//        ));
+//
+//        System.out.println(students.stream().map(Student::getName)
+//                .collect(joining()));
+//        System.out.println(students.stream().map(Student::getName)
+//                .collect(joining(",")));
+//        System.out.println(students.stream().map(Student::getName)
+//                .collect(joining(",","begin","end")));
+        Map<Integer, Map<String, List<Student>>> collect = students.stream().collect(
+                groupingBy(Student::getScore, groupingBy(Student::getName))
+        );
+        System.out.println(collect);
+
+        System.out.println(students.stream().collect(
+                partitioningBy(student -> student.getScore() > 80)
+        ));
+        Map<Boolean, Map<Boolean, List<Student>>> collect1 = students.stream().collect(
+                partitioningBy(student -> student.getScore() > 80,
+                        partitioningBy(student -> student.getScore() > 90))
+        );
+        System.out.println(collect1);
+
+        System.out.println(students.stream().collect(
+                partitioningBy(student -> student.getScore() > 80, counting())
+        ));
+
+        Map<String, Student> collect2 = students.stream().collect(
+                groupingBy(Student::getName,
+                        collectingAndThen(
+                                minBy(Comparator.comparingInt(Student::getScore)),
+                                Optional::get)
+                )
+        );
+        System.out.println(collect2);
+        for (Student student : students) {
+            students.get(0);
+        }
     }
 }
